b5ef775f30c91b971841cefb60e9704ed5c43c96
Lemme <andre.lemme@xamla.com>
2017-10-19 17:12:55 +0200

    lock service and necessary structure

--- xutils/LeasedBaseLockClient.lua
+++ xutils/LeasedBaseLockClient.lua
@@ -15,15 +15,12 @@ end
 end
 
 local function query_lock(self, id_resources, id_lock, release_flag)
-  ros.WARN(string.format("query_lock: id = %s, release = %s", id_lock, release_flag))
+  ros.DEBUG(string.format("query_lock: id = %s, release = %s", id_lock, release_flag))
   local request = self.query_resource_lock_service:createRequest()
   request.release = release_flag or false
   request.id_resources = id_resources
   request.id_lock = id_lock or ''
-  print(request)
-
   local responds = self.query_resource_lock_service:call(request)
-  print(responds)
   if responds.success then
       return responds.success, responds.id_lock, responds.creation_date, responds.expiration_date
   else
@@ -39,8 +36,12 @@ end
 
 function LeasedBaseLockClient:release(lock)
   assert(torch.type(lock) == 'table', "type should be 'table' but is: " .. torch.type(lock))
-  assert(lock.id, "Lock should habe an id but is nil")
+  assert(lock.id, "Lock should have an id but it is nil")
   return query_lock(self, lock.resources, lock.id, true)
+end
+
+function LeasedBaseLockClient:shutdown()
+  self.query_resource_lock_service:shutdown()
 end
 
 return LeasedBaseLockClient
