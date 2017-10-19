30b0930e88d0b1abd3329ca7b2fb18f092b186a8
Lemme <andre.lemme@xamla.com>
2017-10-19 17:12:55 +0200

    lock service and necessary structure

--- components/MoveJSafeSteppingService.lua
+++ components/MoveJSafeSteppingService.lua
@@ -2,11 +2,13 @@ local ros = require 'ros'
 local moveit = require 'moveit'
 local xutils = require 'xamlamoveit.xutils'
 local LeasedBaseLockClient = xutils.LeasedBaseLockClient
+local ms = require 'xamlamoveit.components.MotionService'
 
 local function queryMoveJSafeSteppingServiceHandler(self, request, response, header)
     if not self.current_path then
         self.path_index = 1
-        self.current_path = request.path
+        self.current_goal = request.point
+        self.current_path = nil
         response.success = true
     else
         response.success = false
@@ -15,9 +17,22 @@ local function queryMoveJSafeSteppingServiceHandler(self, request, response, hea
     return true
 end
 
+local function queryNextPathPointHandler(self, request, response, header)
+    if self.path_index > 0 then
+        self.path_index = self.path_index + 1
+        response.success = #self.current_path >= self.path_index
+        response.message =
+            string.format('Path index increased to %d from %d points in path', self.path_index, #self.current_path)
+    else
+        response.success = false
+        response.message = 'No path loaded'
+    end
+end
+
 local set_bool_spec = ros.SrvSpec('std_srvs/SetBool')
 local srv_spec = ros.SrvSpec('xamlamoveit_msgs/SetJointPath')
 local trigger_srv_spec = ros.SrvSpec('std_srvs/Trigger')
+local joint_point_spec = ros.MsgSpec("trajectory_msgs/JointTrajectoryPoint")
 
 local components = require 'xamlamoveit.components.env'
 local MoveJSafeSteppingService,
@@ -30,20 +45,24 @@ function MoveJSafeSteppingService:__init(nh)
     self.trigger_callback_queue = ros.CallbackQueue()
     self.robot_model_loader = nil
     self.robot_model = nil
+    self.resource_names = nil
     self.info_server = nil
     self.trigger_server = nil
     self.current_path = nil
     self.path_index = -1
     self.lock_client = nil
     self.lock = nil
+    self.motion_service = nil
     parent.__init(self, nh)
 end
 
 function MoveJSafeSteppingService:onInitialize()
     self.robot_model_loader = moveit.RobotModelLoader('robot_description')
     self.robot_model = self.robot_model_loader:getModel()
+    local robot_state = moveit.RobotState.createFromModel(self.robot_model)
+    self.resource_names = robot_state:getVariableNames():totable()
     self.lock_client = LeasedBaseLockClient(self.node_handle)
-
+    self.motion_service = ms.new(self.node_handle)
     --TODO feedback
 end
 
@@ -66,9 +85,7 @@ function MoveJSafeSteppingService:onStart()
         end,
         self.trigger_callback_queue
     )
-    local joy_spec = 'std_msg/Bool'
-    self.subscriber_trigger = self.nh:subscribe(topic, joy_spec, 1)
-    self.publisher = self.node_handle:advertise('topic_to_joystick', string_spec, 1)
+    self.publisher = self.node_handle:advertise('topic_to_controller', joint_point_spec, 1)
 end
 
 function MoveJSafeSteppingService:onProcess()
@@ -76,11 +93,15 @@ function MoveJSafeSteppingService:onProcess()
         ros.INFO('[!] incoming MoveJSafeSteppingService call')
         self.callback_queue:callAvailable()
     end
+    if not self.trigger_callback_queue:isEmpty() then
+        ros.INFO('[!] incoming trigger call for MoveJSafeSteppingService')
+        self.trigger_callback_queue:callAvailable()
+    end
     if self.path_index > 0 then
         if self.lock then
-            self.lock_client:lock()
-        else
             self.lock = self.lock_client:lock(self.lock.resources, self.lock.id)
+        else
+            self.lock = self.lock_client:lock(self.resource_names)
         end
         --lock all resources
         if self.path_index == 1 then
@@ -95,14 +116,23 @@ function MoveJSafeSteppingService:onProcess()
 end
 
 function MoveJSafeSteppingService:onReset()
+    self.current_path = nil
+    self.path_index = -1
+    if self.lock then
+        self.lock_client:release(self.lock.resources, self.lock.id)
+        self.lock = nil
+    end
 end
 
 function MoveJSafeSteppingService:onStop()
     self.info_server:shutdown()
+    self.trigger_server:shutdown()
 end
 
 function MoveJSafeSteppingService:onShutdown()
     self.info_server:shutdown()
+    self.trigger_server:shutdown()
+    self.lock_client:shutdown()
 end
 
 return MoveJSafeSteppingService
