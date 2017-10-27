ac219821c31a1ec86dff7ea480c6c64cc542fe2b
Lemme <andre.lemme@xamla.com>
2017-10-27 11:21:45 +0200

    bugfix 2

--- actionNodes/moveJActionServer.lua
+++ actionNodes/moveJActionServer.lua
@@ -1,7 +1,7 @@
 #!/usr/bin/env th
 local ros = require 'ros'
-local motionLibrary = require 'xamlamoveit.motionLibrary'
-local MoveJActionServer = motionLibrary.MoveJActionServer
+local components = require 'xamlamoveit.components'
+local MoveJActionServer = components.MoveJActionServer
 local xutils = require 'xamlamoveit.xutils'
 local xamla_sysmon = require 'xamla_sysmon'
 
