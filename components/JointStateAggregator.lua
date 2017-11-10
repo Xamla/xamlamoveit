d34770174912deb3574abc47563b0409342e632e
Lemme <andre.lemme@xamla.com>
2017-11-10 13:43:02 +0100

    changed to controller list parameter

--- components/JointStateAggregator.lua
+++ components/JointStateAggregator.lua
@@ -37,23 +37,54 @@ end
 function JointStateAggregator:__init(node_handle, timeout)
     self.node_handle = node_handle
     self.callback_queue = ros.CallbackQueue()
-    self.robot_model = nil
-    self.robot_model_loader = nil
-    self.robot_state = nil
     self.joint_state_publisher = nil
     self.joint_monitor = nil
     self.last_joint_state = nil
-    self.joint_names = nil
+    self.joint_names = {}
     self.timeout = toDuration(timeout, ros.Duration(0.1))
     self.feedback_topic = 'aggregated_joint_state'
     self.seq = 1
     parent.__init(self, node_handle)
 end
 
+local function getControllerConfig(self)
+    local config = self.node_handle:getParamVariable(string.format('%s/controller_list', self.node_handle:getNamespace()))
+    --local config = self.node_handle:getParamVariable('/move_group/controller_list')
+    local start_time = ros.Time.now()
+    local current_time = ros.Time.now()
+    local attemts = 0
+    while config == nil do
+        attemts = attemts + 1
+        ros.WARN('no controller specified in "%s/controller_list". Retry in 5sec', self.node_handle:getNamespace())
+        while current_time:toSec() - start_time:toSec() < 5 do
+            current_time = ros.Time.now()
+            sys.sleep(0.01)
+        end
+        start_time = ros.Time.now()
+        current_time = ros.Time.now()
+        config = self.node_handle:getParamVariable(string.format('%s/controller_list', self.node_handle:getNamespace()))
+
+        if not ros.ok() then
+            return -1, 'Ros is not ok'
+        end
+
+        if attemts > 5 then
+            return -2, 'Reached max attempts'
+        end
+    end
+    for i, v in ipairs(config) do
+        print(v)
+        for ii, vv in ipairs(v.joints) do
+            if table.indexof(self.joint_names, vv) == -1 then
+                self.joint_names[#self.joint_names + 1] = vv
+            end
+        end
+    end
+end
+
 function JointStateAggregator:onInitialize()
-    self.robot_model_loader = moveit.RobotModelLoader('robot_description')
-    self.robot_model = self.robot_model_loader:getModel()
-    self.joint_monitor = JointMonitor.new(self.robot_model:getVariableNames():totable(), self.timeout:toSec())
+    getControllerConfig(self)
+    self.joint_monitor = JointMonitor.new(self.joint_names, self.timeout:toSec())
     local ready = self.joint_monitor:waitReady(2.1)
     if ready then
         self.last_joint_state = self.joint_monitor:getPositionsTensor()
