c061e17b2cae73a0be2c1c9e6b7a8f151cf377e6
Jan Nörtemann <jan.noertemann@xamla.com>
2018-02-26 19:09:22 +0100

    Add Weiss two finger simulation action node

---
+++ actionNodes/weissTwoFingerSimulationNode.lua
@@ -0,0 +1,32 @@
+#!/usr/bin/env th
+
+local ros = require 'ros'
+local xamlamoveit = require 'xamlamoveit'
+local grippers = require 'xamlamoveit.grippers.env'
+local xutils = xamlamoveit.xutils
+
+local node_handle
+local function initSetup(ns, param)
+    ros.init(ns, nil, param)
+    node_handle = ros.NodeHandle('~')
+end
+
+local parameter = xutils.parseRosParametersFromCommandLine(arg, cmd) or {}
+initSetup(parameter['__name'], parameter)
+
+local joint_command_namespace = node_handle:getParamString('joint_command_namespace')
+local actuated_joint_name = node_handle:getParamString('actuated_joint_name')
+local action_server = grippers.Weiss2FingerSimulation.new(node_handle, joint_command_namespace, actuated_joint_name)
+
+print('Spinning')
+local rate = ros.Rate(10)
+while ros.ok() do
+  ros.spinOnce()
+  action_server:spin()
+  rate:sleep()
+end
+
+print('Shutdown')
+action_server:shutdown()
+node_handle:shutdown()
+ros.shutdown()
