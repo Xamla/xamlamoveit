ff05fcb1105ca56e4fa60929c223d3e9be5e548e
ialtrogge <inga.altrogge@xamla.com>
2018-03-07 15:13:49 +0100

    Update init.lua

--- grippers/init.lua
+++ grippers/init.lua
@@ -3,4 +3,5 @@ require 'xamlamoveit.grippers.RobotiqCModelXamla'
 require 'xamlamoveit.grippers.GenericRosGripperClient'
 require 'xamlamoveit.grippers.GenericTwoFingerSimulation'
 require 'xamlamoveit.grippers.WeissTwoFingerSimulation'
+require 'xamlamoveit.grippers.WeissTwoFingerModel'
 return grippers
