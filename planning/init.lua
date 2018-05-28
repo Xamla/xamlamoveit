5cd8b906b804e5f6307d573a3506e894d15d9137
Lemme <andre.lemme@xamla.com>
2018-05-28 09:13:33 +0200

    cleanup

--- planning/init.lua
+++ planning/init.lua
@@ -1,7 +1,3 @@
 local planning = require 'xamlamoveit.planning.env'
 require 'xamlamoveit.planning.Pattern'
-require 'xamlamoveit.planning.MoveitPlanning'
-
-
---Planning.x3d = require 'xamlamoveit.Planning.x3d'
 return planning
