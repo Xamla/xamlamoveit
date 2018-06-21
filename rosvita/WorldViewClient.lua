3fe5193864bcab0aed6786c58709a6873c5aaca3
Lemme <andre.lemme@xamla.com>
2018-06-21 17:59:42 +0200

    handling of cartesianpath in worldview

--- rosvita/WorldViewClient.lua
+++ rosvita/WorldViewClient.lua
@@ -28,11 +28,17 @@ local UPDATE_POSE_SERVER_ADDRESS = '/rosvita/world_view/update_pose'
 local REMOVE_SERVER_ADDRESS = '/rosvita/world_view/remove_element'
 local ADD_FOLDER_SERVER_ADDRESS = '/rosvita/world_view/add_folder'
 
+local UPDATE_CARTESIANPATH_SERVER_ADDRESS = '/rosvita/world_view/update_cartesianpath'
+local SET_CARTESIANPATH_SERVER_ADDRESS = '/rosvita/world_view/set_cartesianpath'
+local GET_CARTESIANPATH_SERVER_ADDRESS = '/rosvita/world_view/get_cartesianpath'
+local QUERY_CARTESIANPATH_SERVER_ADDRESS = '/rosvita/world_view/query_cartesianpath'
+
 local ros = require 'ros'
 local datatypes = require 'xamlamoveit.datatypes'
 local rosvita = require 'xamlamoveit.rosvita.env'
 
 local joint_values_point_spec = ros.MsgSpec('xamlamoveit_msgs/JointValuesPoint')
+local cartesian_path_spec = ros.MsgSpec('xamlamoveit_msgs/CartesianPath')
 
 local get_joint_posture_spec = ros.SrvSpec('xamlamoveit_msgs/GetJointPostureWorldView')
 local set_joint_posture_spec = ros.SrvSpec('xamlamoveit_msgs/SetJointPostureWorldView')
@@ -44,6 +50,9 @@ local remove_element_spec = ros.SrvSpec('xamlamoveit_msgs/RemoveElementWorldView
 local create_folder_spec = ros.SrvSpec('xamlamoveit_msgs/CreateFolderWorldView')
 local query_poses_spec = ros.SrvSpec('xamlamoveit_msgs/QueryPosesWorldView')
 local query_joint_postures_spec = ros.SrvSpec('xamlamoveit_msgs/QueryJointValuesWorldView')
+local query_cartesianpath_spec = ros.SrvSpec('xamlamoveit_msgs/QueryCartesainPathWorldView')
+local set_cartesianpath_spec = ros.SrvSpec('xamlamoveit_msgs/SetCartesianPathWorldView')
+local get_cartesianpath_spec = ros.SrvSpec('xamlamoveit_msgs/GetCartesianPathWorldView')
 
 local WorldViewClient = torch.class('xamlamoveit.rosvita.WorldViewClient', rosvita)
 
@@ -60,6 +69,11 @@ function WorldViewClient:__init(node_handle)
     self.update_pose = self.node_handle:serviceClient(UPDATE_POSE_SERVER_ADDRESS, update_pose_spec)
     self.remove_element = self.node_handle:serviceClient(REMOVE_SERVER_ADDRESS, remove_element_spec)
     self.create_folder = self.node_handle:serviceClient(ADD_FOLDER_SERVER_ADDRESS, create_folder_spec)
+
+    self.update_cartesianpath = self.node_handle:serviceClient(UPDATE_CARTESIANPATH_SERVER_ADDRESS, set_cartesianpath_spec)
+    self.set_cartesianpath = self.node_handle:serviceClient(SET_CARTESIANPATH_SERVER_ADDRESS, set_cartesianpath_spec)
+    self.get_cartesianpath = self.node_handle:serviceClient(GET_CARTESIANPATH_SERVER_ADDRESS, get_cartesianpath_spec)
+    self.query_cartesianpaths = self.node_handle:serviceClient(QUERY_CARTESIANPATH_SERVER_ADDRESS, query_cartesianpath_spec)
 end
 
 function WorldViewClient:getJointPosture(element_path)
@@ -225,6 +239,122 @@ function WorldViewClient:queryPoses(prefix, folder_path, recursive)
     return false, nil, 'service not valid'
 end
 
+function WorldViewClient:queryCartesianPath(prefix, folder_path, recursive)
+    assert(torch.type(prefix) == 'string', 'element_path should be a string')
+    folder_path = folder_path or ''
+    recursive = recursive or false
+    local request = self.query_cartesianpaths:createRequest()
+    request.prefix = prefix
+    request.folder_path = folder_path
+    request.recursive = recursive
+
+    if self.query_cartesianpaths:isValid() then
+        local responds = self.query_cartesianpaths:call(request)
+        if responds then
+            if responds.success then
+                local result = {}
+                for i, v in ipairs(responds.paths) do
+                    local path = {}
+                    for ii, vv in ipairs(v.points) do
+                        path[#path + 1] = poseFromPoseStampedMsg(vv)
+                    end
+                    result[#result + 1] = path
+                end
+                return responds.success, result, responds.error
+            else
+                return responds.success, nil, responds.error
+            end
+        end
+    end
+    return false, nil, 'service not valid'
+end
+
+function WorldViewClient:getCartesianPath(element_path)
+    assert(torch.type(element_path) == 'string', 'element_path should be a string')
+    assert(element_path ~= '', 'element_path should not be an empty string')
+    local request = self.get_cartesianpath:createRequest()
+    request.element_path = element_path
+    if self.get_cartesianpath:isValid() then
+        local responds = self.get_cartesianpath:call(request)
+        if responds then
+            if responds.success then
+                local result = {}
+                for i, v in ipairs(responds.path.points) do
+                    result[#result + 1] = poseFromPoseStampedMsg(v)
+                end
+                return responds.success, result, responds.error
+            else
+                return responds.success, nil, responds.error
+            end
+        end
+    end
+    return false, nil, 'service not valid'
+end
+
+function WorldViewClient:addCartesianPath(display_name, element_path, path, transient)
+    assert(torch.type(path) == 'table', 'element_path should be a table with datatypes.Pose elements')
+    assert(torch.type(display_name) == 'string', 'element_path should be a string')
+    assert(display_name ~= '', 'element_path should not be an empty string')
+
+    element_path = element_path or ''
+    transient = transient or false
+    local request = self.set_cartesianpath:createRequest()
+    request.element_path = element_path
+    request.display_name = display_name
+    request.transient = transient
+    local points = {}
+    for i, point in ipairs(path) do
+        assert(torch.isTypeOf(point, datatypes.Pose), 'Should be xamlamoveit.datatypes.Pose but is:' .. torch.type(point))
+        points[#points + 1] = point:toStampedPoseMsg()
+    end
+    request.path = ros.Message(cartesian_path_spec)
+    request.path.points = points
+
+    if self.set_cartesianpath:isValid() then
+        local responds = self.set_cartesianpath:call(request)
+        if responds then
+            if responds.success then
+                return responds.success, responds.error
+            else
+                return responds.success, responds.error
+            end
+        end
+    end
+    return false, 'service not valid'
+end
+
+function WorldViewClient:updateCartesianPath(display_name, element_path, path, transient)
+    assert(torch.type(path) == 'table', 'element_path should be a table with datatypes.Pose elements')
+    assert(torch.type(display_name) == 'string', 'element_path should be a string')
+    assert(display_name ~= '', 'element_path should not be an empty string')
+
+    element_path = element_path or ''
+    transient = transient or false
+    local request = self.update_cartesianpath:createRequest()
+    request.element_path = element_path
+    request.display_name = display_name
+    request.transient = transient
+    local points = {}
+    for i, point in ipairs(path) do
+        assert(torch.isTypeOf(point, datatypes.Pose), 'Should be xamlamoveit.datatypes.Pose but is:' .. torch.type(point))
+        points[#points + 1] = point:toStampedPoseMsg()
+    end
+    --request.path = ros.Message(cartesian_path_spec)
+    request.path.points = points
+    print(request.path)
+    if self.update_cartesianpath:isValid() then
+        local responds = self.update_cartesianpath:call(request)
+        if responds then
+            if responds.success then
+                return responds.success, responds.error
+            else
+                return responds.success, responds.error
+            end
+        end
+    end
+    return false, 'service not valid'
+end
+
 function WorldViewClient:queryJointValues(prefix, folder_path, recursive)
     assert(torch.type(prefix) == 'string', 'element_path should be a string')
     folder_path = folder_path or ''
@@ -319,6 +449,14 @@ local function removeElement(self, element_path)
     return false, 'service not valid'
 end
 
+function WorldViewClient:removeCartesianPath(display_name, element_path)
+    assert(torch.type(display_name) == 'string', 'element_path should be a string')
+    assert(display_name ~= '', 'element_path should not be an empty string')
+    local element_path = element_path or ''
+    local path = paths.concat('/', element_path, display_name)
+    return removeElement(self, path)
+end
+
 function WorldViewClient:removePose(display_name, element_path)
     assert(torch.type(display_name) == 'string', 'element_path should be a string')
     assert(display_name ~= '', 'element_path should not be an empty string')
@@ -362,6 +500,10 @@ function WorldViewClient:shutdown()
     disposeRos(self, 'create_folder')
     disposeRos(self, 'query_poses')
     disposeRos(self, 'query_joint_postures')
+
+    disposeRos(self, 'set_cartesianpath')
+    disposeRos(self, 'get_cartesianpath')
+    disposeRos(self, 'query_cartesianpaths')
 
     assert(self.update_pose == nil)
     assert(self.query_joint_postures == nil)
